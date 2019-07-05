18d5b26b34d7c28582df3bbfb4b41f2e8607a796
fishouk <fishouk@yandex.ru>
2019-07-05 14:47:55 +0300

    modified files and add search inout for list of names

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import {Row, Col, Container, ListGroup, Form, InputGroup, FormControl} from 'react-bootstrap';
 import React, {Component} from 'react';
 import './App.css';
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-class App extends Component {
-    constructor() {
-      super();
+class App extends Component { 
+
+    constructor(props) {
+      super(props);
       this.state = {
-        data: [],
-        isLoading: false,
-        error: null,
-      }
+          searchString: "",
+          data: [],
+          isLoading: false,
+          error: null,
+      };
+      this.formListOfNamesSearch = this.formListOfNamesSearch.bind(this);
     }
+
     componentDidMount() {
       this.setState({ isLoading: true });
 
@@ -25,46 +30,75 @@ class App extends Component {
         })
         .then(data => this.setState({ data: data, isLoading: false }))
         .catch(error => this.setState({ error, isLoading: false }));
+      
+        this.refs.searchName.focus();
     }
-    render() {
-      const { data, isLoading, error } = this.state;
-      if (error) {
-        return (
-          <div className="App">
-            <h1 className="title">Get list of names by api with react app</h1>
-            <p>{error.message}</p>
-          </div>  
-        );
-      }
+  
+    formListOfNamesSearch() {
+      this.setState({
+        searchString: this.refs.searchName.value
+      });
+    }
 
-      if (isLoading) {
-        return (
-          <div className="App">
-            <h1 className="title">Get list of names by api with react app</h1>
-            <p>Loading ...</p>
-          </div>
-        );
+    render() {
+      const { isLoading, error } = this.state;
+      let data = this.state.data;
+      let search = this.state.searchString.trim().toLowerCase();
+  
+      if (search.length > 0) {
+          data = data.filter(function(data) {
+          return data.name.toLowerCase().match(search);
+        });
       }
 
       return (    
-        <div className="App">
-          <h1 className="title">Get list of names by api with react app</h1>
-          <div className="columns">
-            <div className="column is-12">
-            <ul>
-              {data.map(hit =>
-                <li key={hit.id}>
-                  {hit.name}
-                </li>
-              )}
-            </ul>
-            </div>
-          </div>
-        </div>
+          <Container>
+            <Row>
+              <Col>
+                <h1>Get list of names by api with react app</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form>
+                  <Form.Group controlId="FormListOfNamesSearch">
+                    <Form.Label>Поиск</Form.Label>
+                    <InputGroup className="mb-3">
+                      <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">Search</InputGroup.Text>
+                      </InputGroup.Prepend>
+                      <FormControl
+                        value={this.state.searchString}
+                        ref="searchName"
+                        placeholder="Начните печатать"
+                        onChange={this.formListOfNamesSearch}
+                      />
+                    </InputGroup>
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+              {error ? <p>{error.message}</p> : null}
+              {!isLoading ? (
+                <ListGroup variant="flush">
+                  {data.map(hit => {
+                    return(
+                      <ListGroup.Item key={hit.id}>
+                        {hit.name}
+                      </ListGroup.Item>
+                      );
+                    })
+                  }
+                </ListGroup>
+                ) : (
+                <p>Loading ...</p>)}
+              </Col>
+            </Row>
+          </Container>
       );  
     }
 }
-
-
 
 export default App;
